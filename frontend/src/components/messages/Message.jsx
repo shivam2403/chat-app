a33eaa6a74e9dbb69a65836159e70088c6c0ea71
Shivam <ss6928228@gmail.com>
2024-03-07 20:36:10 +0530

    removed extra padding

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -14,7 +14,7 @@ const Message = ({ message }) => {
 	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
-		<div className={`chat ${chatClassName} p-4`}>
+		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
